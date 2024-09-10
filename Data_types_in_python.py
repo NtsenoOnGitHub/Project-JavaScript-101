64cb1a25960bf9c75fae83e6bea354a0eda23133
Ntseno Ntseno <ntseno.ntseno@gmail.com>
2024-09-10 21:20:35 +0200

    deifined python data types

---
+++ Data_types_in_python.py
@@ -0,0 +1,8 @@
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Boolean Type:	bool
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
