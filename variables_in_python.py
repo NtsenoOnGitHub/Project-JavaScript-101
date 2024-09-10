9ce50f6de91e844afdb1f388ece51dd8086d384e
Ntseno Ntseno <ntseno.ntseno@gmail.com>
2024-09-10 20:49:58 +0200

    modified the file

--- variables_in_python.py
+++ variables_in_python.py
@@ -1,4 +1,23 @@
 name = 'Ntseno' # name is a string
 x = 5 # X is an integer 
 y = True # y is a Boolean 
-z = 5.0 # z is a double
+z = 5.0 # z is a float
+
+# Casting / Specifying the Variable data type
+e = float(5)
+f = int(5.0)
+
+print(f)
+
+# Multi word varible (Naming styles)
+myVarible = 'Camel Casing'
+MyVariable = 'Pascal Casing'
+my_variable = 'Snake Casing'
+
+# outputing variables in the print() method
+print(myVarible, ' ' , z) # variables of various data types are outputed separated by commas inside the the print()
+print(my_variable + ' ' + myVarible) # You can also use the + operator to output multiple variables:
+print(z + x) # For numbers, the + character works as a mathematical operator
+
+
+
